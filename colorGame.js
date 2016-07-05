020c717fa6ac28f0ed09d7ea4012ad2087427f2e
jess-alexander <alexanderjessmarie@gmail.com>
2016-07-05 11:36:58 +0200

    Add "random color" functionality
    add in functions to create and display random colors to page.

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,28 @@
-var colorsArray = [ //hard coded for now - randomize later
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 0, 255)",
-	"rgb(0, 255, 255)",
-	"rgb(255, 0, 255)"
-	]
+var colorsArray = generateRandomColors(6);  //hard coded for now - randomize later
+
 
 var squares = document.querySelectorAll(".square"); //each square to be colored
 var pickedColor = pickColor(); // change name to "winning color" instead?
 var colorDisplay = document.getElementById("colorDisplay"); //span to display winning color
 var messageDisplay = document.querySelector("#message");// span to display "correct"/"try again"
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var hardButton = document.querySelector("#hard");
+var easyButton = document.getElementById("easy");
+
+resetButton.addEventListener("click", function(){
+	colorsArray = generateRandomColors(6);//generate all new colors
+	pickedColor = pickColor();// pick a new random color from array
+	colorDisplay.textContent = pickedColor;//change color display to match picked Color;
+	
+	for(var i = 0; i < squares.length; i++){     //assign color to squares
+		squares[i].style.background = colorsArray[i]; //change colros of squares on page;
+		} 
+	resetButton.textContent = "New Colors";
+	messageDisplay.textContent = "";
+	h1.style.background = "#232323";
+
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,6 +35,8 @@ for(var i = 0; i < squares.length; i++){
 		if(clickedColor === pickedColor){//compare color to pickedColor
 			messageDisplay.textContent = "Correct";
 			changeColors(clickedColor);//change all squares to winning color
+			h1.style.background = clickedColor;
+			resetButton.textContent = "Play Again?";
 		} else {
 			this.style.background = "#232323"; //change visibility to hide incorrect selection
 			messageDisplay.textContent = "Try Again"; //add in display of "try again"
@@ -49,4 +63,23 @@ function pickColor(){ //pick winning square -- not randomize color
 	
 }
 
-	
+
+function generateRandomColors(num){
+
+	var arr = []; //make an array
+	for(var i = 0; i < num; i++){ //repeat num times
+		
+		arr.push(randomColor())//get random color & push into array
+	}	
+	// add num random colors to array
+	return arr;//return that array
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random() * 256); //pick a "red" from 0 - 255
+	var g = Math.floor(Math.random() * 256);//pick a "green" from 0 - 255
+	var b = Math.floor(Math.random() * 256);//pick a "blue" from 0 - 255
+	//rgb("r, g, b");
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+
+}
